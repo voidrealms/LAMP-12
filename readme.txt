249f08bf625b62ebb04c5d55bd3591b00ca022af
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:26:10 -0400

    First commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: LAMP 12 - IF, ELSEIF and ELSE
+Published: 11/3/2012 1:48:41 PM
+Link: https://www.youtube.com/watch?v=m8IdMDFzqFs
+
+Description: Learn how to use IF, ELSEIF and ELSE to control the flow of a program
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
---
+++ tut12.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+$age = 55;
+
+if ($age >= 65)
+{
+	echo "You are retired";
+}
+elseif ($age >= 55)
+{
+	echo "You are almost retired!";
+}
+else
+{
+	echo "You are not retired";
+}
+
+?>
+
+</body>
+</html>
